0c2c28b2ce66673d33a4fc6d932fc4c176d749b8
arsalanmrz <a.mirbozorgi@medrick.info>
2021-09-07 17:26:10 +0430

    add

---
+++ src/main/java/com/mirbozorgi/springshell/demo/MyCommands.java
@@ -0,0 +1,21 @@
+package com.mirbozorgi.springshell.demo;
+
+import javax.validation.constraints.Size;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class MyCommands {
+
+  @ShellMethod(value = "Add numbers.", key = "sum")
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  @ShellMethod(value = "Change password.", key = "changePassword")
+  public String changePassword(@Size(min = 8, max = 40) String password) {
+    return "Password successfully set to " + password;
+  }
+
+
+}
---
+++ src/test/java/com/mirbozorgi/springshell/demo/SpringshellDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirbozorgi.springshell.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringshellDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
